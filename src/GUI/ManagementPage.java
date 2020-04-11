ae0823be4eae605469bef157320b27ee2cb70a45
edgax <40456087+edgax@users.noreply.github.com>
2020-04-11 16:53:49 +0100

    Adding function for Page GamePage,RemovePage

--- src/GUI/ManagementPage.java
+++ src/GUI/ManagementPage.java
@@ -311,7 +311,7 @@ public class ManagementPage extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         new HomePage().setVisible(true);
-
+            System.out.println("HELLO");
         this.dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
